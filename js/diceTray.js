18206f38d4b457a3562a791c5a27f2aadd81d4c1
ronansky <ronandrost20@gmail.com>
2026-03-24 23:19:54 +0100

    Refactor diceTray function for consistency and readability

--- js/diceTray.js
+++ js/diceTray.js
@@ -1,29 +1,28 @@
 import { rollDice } from "./diceRoller.js";
 
 export function diceTray() {
-    const diceTray = document.getElementById("diceTrayDiv")
+    const diceTray = document.getElementById("diceTrayDiv");
 
-    const diceArray = [20, 6, 8, 4, 10, 12]
+    const diceArray = [20, 6, 8, 4, 10, 12];
 
     for (let index = 0; index < diceArray.length; index++) {
         const diceTrayButtonIndex = diceArray[index];
 
-        const indDiceTrayButton = document.createElement("button")
-        indDiceTrayButton.textContent = `d${diceTrayButtonIndex}`
+        const indDiceTrayButton = document.createElement("button");
+        indDiceTrayButton.textContent = `d${diceTrayButtonIndex}`;
 
         indDiceTrayButton.addEventListener("click", async (event) => {
-            const diceRollResult = document.createElement("div")
-            const clickedElement = event.target
-            console.log(event.target)
+            const diceRollResult = document.createElement("div");
+            const clickedElement = event.target;
+            console.log(event.target);
 
-            const rollResult = await rollDice(diceTrayButtonIndex)
-            console.log(rollResult)
+            const rollResult = await rollDice(diceTrayButtonIndex);
+            console.log(rollResult);
 
-            diceRollResult.textContent = `Your d${diceTrayButtonIndex} rolled a: ${rollResult.total}`
-            diceTray.appendChild(diceRollResult)
-        })
+            diceRollResult.textContent = `Your d${diceTrayButtonIndex} rolled a: ${rollResult.total}`;
+            diceTray.appendChild(diceRollResult);
+        });
 
-        diceTray.appendChild(indDiceTrayButton)
-
+        diceTray.appendChild(indDiceTrayButton);
     }
 }
